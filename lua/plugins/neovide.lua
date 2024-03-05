a7cec354da26e654764e777f5ee32e042a80f056
sig <corynz@pm.me>
2024-03-05 08:18:33 +0000

    initial nvchad commit

---
+++ lua/plugins/neovide.lua
@@ -0,0 +1,10 @@
+
+
+if vim.g.neovide then
+  vim.g.neovide_padding_top = 10
+  vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+
+  vim.g.neovide_refresh_rate = 144
+end
